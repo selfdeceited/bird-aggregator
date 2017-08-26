b74b9850ba36f316ea9cefdc4c089c81f84a513a
selfdeceited <selfdeceited@gmail.com>
2017-08-26 20:30:02 +0300

    (*) map UI changes

--- src/components/MapWrap.tsx
+++ src/components/MapWrap.tsx
@@ -1,13 +1,14 @@
 import * as React from "react"
 import UnderConstructionState from './UnderConstructionState'
 import axios from 'axios';
-import ReactMapboxGl, { Layer, Feature, ZoomControl } from "react-mapbox-gl"
+import ReactMapboxGl, { Layer, Feature, ZoomControl, Popup } from "react-mapbox-gl"
 
 const Map = ReactMapboxGl({
     accessToken: "pk.eyJ1IjoidG9ueXJ5emhpa292IiwiYSI6ImNpbHhvYTY0MDA4MTF0bWtyaW9xbjAyaWsifQ.ih-8rDMRiBmDPqdeyyrHNg"
 });
 
-//todo: fix https://github.com/mapbox/mapbox-gl-js/issues/2440
+// todo: fix https://github.com/mapbox/mapbox-gl-js/issues/2440
+// enhance according to: http://alex3165.github.io/react-mapbox-gl/demos
 
 interface MapWrapProps {}
 interface MapWrapState {
@@ -39,15 +40,19 @@ export class MapWrap extends React.Component<MapWrapProps, MapWrapState> {
         return (
 <div className="body">
 <Map
-  style="mapbox://styles/mapbox/streets-v9"
+  style="mapbox://styles/mapbox/outdoors-v10"
   containerStyle={{
     height: "100vh",
     width: "100vw"
-  }}>
+  }}
+  center={[35.5, 55.6]}
+  zoom={[6]}
+  >
     <Layer
       type="symbol"
       id="marker"
-      layout={{ "icon-image": "marker-15" }}>
+      layout={{ "icon-image": "circle-15" }}
+      >
 
       { this.state.markers.map(x => (
         <Feature
