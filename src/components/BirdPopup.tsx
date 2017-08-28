4159dea9e6483f867dc5573d2549ecebac13130a
selfdeceited <selfdeceited@gmail.com>
2017-08-28 09:41:56 +0300

    (*) fixed popup

--- src/components/BirdPopup.tsx
+++ src/components/BirdPopup.tsx
@@ -6,30 +6,31 @@ interface BirdPopupProps {
     birds: BirdDto[]
 }
 interface BirdPopupState {
-    birds: BirdDto[]
+
 }
 
 export class BirdPopup extends React.Component<BirdPopupProps, BirdPopupState> {
 
     constructor(props) {
         super(props);
-        this.state = {
-            birds: []
-        };
+        this.state = {};
     }
 
-    componentWillReceiveProps(nextProps) {
-        this.setState({ birds: nextProps.birds });
-    }
-
-    //todo: fix when links does not render
     render() {
         return (<div>
+                    <p>Birds found:</p>
+                    <div>
                     {
-                        this.state.birds.map(x => {
-                        <Link key={x.id} to={"/birds/" + x.id} role="button" className="pt-button pt-minimal pt-icon-arrow-right">{x.name}</Link>
-                        })
+                        this.props.birds.map(x => (<Link 
+                                key={x.id}
+                                to={"/birds/" + x.id}
+                                role="button"
+                                className="pt-button pt-minimal pt-icon-arrow-right">
+                                    {x.name}
+                            </Link>
+                        ))
                     }
+                    </div>
                 </div>);
     }
 }
