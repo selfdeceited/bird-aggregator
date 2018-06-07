61b1eb855a4ab2e24974ae2e4c7d5a5612c85d0b
selfdeceited <selfdeceited@gmail.com>
2018-06-07 11:33:50 +0300

    (*) deployment scripts updates

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 docker build -t birds .
 docker tag birds registry.heroku.com/bird-aggregator/web
-docker push registry.heroku.com/bird-aggregator/web
+heroku container:login
+heroku container:push web -a bird-aggregator
+heroku container:release web -a bird-aggregator
